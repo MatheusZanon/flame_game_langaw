808013e07996d0c96b0e496142883f6f0113a92f
Matheus <mzblannes@outlook.com>
2019-08-13 09:22:56 -0300

    setup do game loop

---
+++ lib/langaw_game.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+import 'package:flame/game.dart';
+
+class LangawGame extends Game {
+  Size screenSize;
+  
+  void render(Canvas canvas) {
+
+  }
+
+  void update(double t) {
+
+  }
+
+  void resize(Size tamanho) {
+
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flame/util.dart';
+import 'package:flame_game_langaw/langaw_game.dart';
 
 void main() async {
   Util flameUtil = Util();
   await flameUtil.fullScreen();
   await flameUtil.setOrientation(DeviceOrientation.portraitUp);
 
+  LangawGame game = LangawGame();
+  runApp(game.widget);
 
 }
