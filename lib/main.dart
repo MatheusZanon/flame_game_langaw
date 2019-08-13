d951c503f9cc649caedd01e49061a30d71cb8af0
Matheus <mzblannes@outlook.com>
2019-08-13 09:15:32 -0300

    setup inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flame/util.dart';
 
+void main() async {
+  Util flameUtil = Util();
+  await flameUtil.fullScreen();
+  await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+
+
+}
