e2df6a33285a2b22a98f8e5fc4c5154149e1982c
Matheus <mzblannes@outlook.com>
2019-08-14 10:07:44 -0300

    adicionei componente mosca e aleatoreidade de spawn

--- lib/langaw_game.dart
+++ lib/langaw_game.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import 'dart:math';
 import 'package:flame/game.dart';
 import 'package:flame/flame.dart';
 import 'package:flame_game_langaw/componentes/moscas.dart';
@@ -7,20 +8,23 @@ class LangawGame extends Game {
   Size tamanTela;
   double tamanTelha;
   List<Mosca> moscas;
-
+  Random rnd;
   
-  LangawGame() {
-    initialize();
-  }
+  LangawGame() {          
+    initialize();  // constructor necessario pra chamar o método initialize,                   
+  }                // que por ser assincrono nao pode ser iniciado diretamente no constructor
 
   void initialize() async {
     moscas = List<Mosca>();
+    rnd = Random();
     resize(await Flame.util.initialDimensions());
     spawnMosca();  
   } 
 
   void spawnMosca(){
-    moscas.add(Mosca(this, 50, 50));
+    double x = rnd.nextDouble() * (tamanTela.width - tamanTelha);
+    double y = rnd.nextDouble() * (tamanTela.height - tamanTelha);
+    moscas.add(Mosca(this, x, y));
   }
   
   void render(Canvas canvas) {
@@ -28,14 +32,14 @@ class LangawGame extends Game {
     Paint backgPaint = Paint();
     backgPaint.color = Color(0xff8e44ad);
     canvas.drawRect(backgRect, backgPaint);
-    moscas.forEach((Mosca mosca) {mosca.render(canvas);} ); 
+    moscas.forEach((Mosca mosca) {mosca.render(canvas);} ); //para cada mosca, renderiza o canvas
     // forEach precisa tem o formato padrao de conter um parametro para cada item da lista que vai no (), 
     // e uma function também para cada item da lista que vai no {}.
     // ((Mosca mosca) {mosca.render(canvas);} ); poderia ter sido escrito como > ((Mosca mosca) => mosca.render(canvas));
   }
 
   void update(double t) {    
-   moscas.forEach((Mosca mosca) {mosca.update(t);} );
+   moscas.forEach((Mosca mosca) {mosca.update(t);} ); 
   }
 
   void resize(Size tamanho) {
