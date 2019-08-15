63e7b274e648389ed1d1dd89b4c85441db1fe3cc
Matheus <mzblannes@outlook.com>
2019-08-15 12:04:08 -0300

    adicionando imagens e componente jardim

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/gestures.dart';
+import 'package:flame/flame.dart';
 import 'package:flame/util.dart';
 import 'package:flame_game_langaw/langaw_game.dart';
 
@@ -8,6 +9,24 @@ void main() async {
   Util flameUtil = Util();
   await flameUtil.fullScreen();
   await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+  Flame.images.loadAll(<String>[
+    'background/backyard.png',
+    'moscas/agile-fly-1.png',
+    'moscas/agile-fly-2.png',
+    'moscas/agile-fly-dead.png',
+    'moscas/drooler-fly-1.png',
+    'moscas/drooler-fly-2.png',
+    'moscas/drooler-fly-dead.png',
+    'moscas/house-fly-1.png',
+    'moscas/house-fly-2.png',
+    'moscas/house-fly-dead.png',
+    'moscas/hungry-fly-1.png',
+    'moscas/hungry-fly-2.png',
+    'moscas/hungry-fly-dead.png',
+    'moscas/macho-fly-1.png',
+    'moscas/macho-fly-2.png',
+    'moscas/macho-fly-dead.png',
+  ]);
 
   LangawGame game = LangawGame();
   runApp(game.widget);
