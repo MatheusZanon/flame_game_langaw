76198b57864a33b4510f67c2b87a5acfd4ebe026
Matheus <mzblannes@outlook.com>
2019-08-20 10:48:11 -0300

    movimento da mosca

--- lib/componentes/moscas.dart
+++ lib/componentes/moscas.dart
@@ -10,8 +10,18 @@ class Mosca {
   List<Sprite> moscaVoandoListaSprite;
   Sprite mortaSprite;
   double voandoSpriteIndex = 0;
+  Offset destinoAlvo;
+  double get velocidade => game.tamanTelha * 3;
 
-  Mosca(this.game);
+  Mosca(this.game) {
+    determinarAlvo();
+  }
+
+  void determinarAlvo() {
+    double x = game.rnd.nextDouble() * (game.tamanTela.width - (game.tamanTelha * 2.025));
+    double y = game.rnd.nextDouble() * (game.tamanTela.height - (game.tamanTelha * 2.025));
+    destinoAlvo = Offset(x, y);
+  } 
 
   void render(Canvas c) {
     if (taMorta) {
@@ -30,18 +40,31 @@ class Mosca {
       }
     } 
     else {
-       voandoSpriteIndex += 30 * t;
-        if (voandoSpriteIndex >= 2) {
-          voandoSpriteIndex -= 2;        
-        }
+      //bater asas
+      voandoSpriteIndex += 30 * t;
+      if (voandoSpriteIndex >= 2) {
+        voandoSpriteIndex -= 2;        
+      }
+      //mover a mosca
+      double distanciaPasso = velocidade * t;
+      Offset paraDestino = destinoAlvo - Offset(moscaRect.left, moscaRect.top);
+      if(distanciaPasso < paraDestino.distance) {
+        Offset voaParaDestino = Offset.fromDirection(paraDestino.direction, distanciaPasso);
+        moscaRect = moscaRect.shift(voaParaDestino);
+      }
+      else {
+        moscaRect = moscaRect.shift(paraDestino);
+        determinarAlvo();
+      }
     } 
     //translate é uma propriedade da classe Rect que recria o retangulo
     //as novas coordenadas especificadas, no caso, 0 representa X e
     //game.tamanTelha * 8 * t representa Y por meio da formula da variavel t de deslocamento
   }                                                                  
-
+  
   void onTapDown() {
     taMorta = true;
     game.spawnMosca();
   }
+
 }
